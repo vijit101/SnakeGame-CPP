a59e61669c904fb37e8f669191a47b9b51b8f9f0
Shubham <40598861+vijit101@users.noreply.github.com>
2021-05-08 15:11:51 +0530

    Added Snake Input and Score

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 // class
 class Vector2{
@@ -13,27 +14,38 @@ int mapWidth = 20;
 int mapHeight = 20; 
 char mapBorder = '#';
 char snakeHead = 'O';
-char devilFruit = '*';
+char snakeBody = 'x';
+char devilFruit = '+';
 int score;
 Vector2 snakePos,fruitPos;
 enum Dir{ STOP = 0 , LEFT = 1 , RIGHT = 2 , UP = 3 , DOWN = 4};
 Dir snakeDir;
 
 // Functions
+void SpawnFruitAtRandom(){
+    fruitPos.x = rand() % (mapWidth -3); // -3 as mapheight is from 0-18 as I already draw 1st and last row myself in other loop so 20 - 2 = 18
+    fruitPos.y = rand() % (mapHeight -3);// so because of - 3 it will remain inside the map and also prevent it from max boundary
+    // fruit not to spawn on boundary 
+    if(fruitPos.x == 0 || fruitPos.y == 0){
+        SpawnFruitAtRandom();
+    }
+}
 void Setup(){
     gameOver = false;
     snakeDir = STOP;
     // initial position for snake and fruit
     snakePos.x = mapWidth/2;
     snakePos.y = mapHeight/2;
-    fruitPos.x = rand() % mapWidth;
-    fruitPos.y = rand() % mapHeight;
+    SpawnFruitAtRandom();
+    //cout<<"x"<<fruitPos.x<<"y"<<fruitPos.y<<"\n";
     score = 0;
 }
 
 void Draw(){
     // sends command to console to clear screen
     system("cls");
+    //cout<<"x"<<fruitPos.x<<"y"<<fruitPos.y<<"\n";
+    cout<<"Score :"<<score<<endl;
     // prints row 1 as *******
     for(int i=0; i <mapWidth;i++){
         cout<<mapBorder;
@@ -43,7 +55,7 @@ void Draw(){
     for(int i=0; i <mapHeight -2;i++){
         for(int j=0; j <mapWidth;j++){
             if(j == 0 || j == mapWidth -1){
-                cout<<mapBorder;
+                cout<<mapBorder;//mapborder
             } 
             else{
                 if(i == snakePos.x && j == snakePos.y){
@@ -65,11 +77,68 @@ void Draw(){
 }
 
 void Input(){
-
+    // check keyboard input
+    if(_kbhit()){
+        switch (_getch())
+        {
+            case 'w':
+            snakeDir = UP;
+            break;
+            case 'a':
+            snakeDir = LEFT;
+            break;
+            case 's':
+            snakeDir = DOWN;
+            break;
+            case 'd':
+            snakeDir = RIGHT;
+            break;
+            case 'q':
+            gameOver = true;
+            break;
+            default:
+            break;
+        }
+    }
 }
 
 void Logic(){
-
+switch (snakeDir)
+{
+    // here the reason is that i is the row and we are moving snake in i so
+    //row 1 2 3 4 5 6
+    //    2
+    //    3
+    //    4  
+    // x = row which is actually the y axis as I as developer set snakepos.x as
+    // if(i == snakePos.x && j == snakePos.y){
+    //                cout<<snakeHead;
+    //           }
+    // to make it right i == snakepos.y so now row is snakeposy and j is col so j is x
+    case UP:
+    snakePos.x--;
+    break;
+    case DOWN:
+    snakePos.x++;
+    break;
+    case LEFT:
+    snakePos.y--;
+    break;
+    case RIGHT:
+    snakePos.y++;
+    break;
+    default:
+    break;
+    }
+    // map check for snake to hit boundary 
+    if(snakePos.x > mapHeight || snakePos.x <0 || snakePos.y >mapWidth || snakePos.y <0){
+        gameOver = true;
+    }
+    // eat fruit logic 
+    if(snakePos.x == fruitPos.x && snakePos.y == fruitPos.y){
+        score ++;
+        SpawnFruitAtRandom();
+    }
 }
 
 int main()
@@ -81,5 +150,7 @@ int main()
         Input();
         Logic();
     }
+    cout<<"Game Over your Score is :"<<score;
+    
     return 0;
 }
